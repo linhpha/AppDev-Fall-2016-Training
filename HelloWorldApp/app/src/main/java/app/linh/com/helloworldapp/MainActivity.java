57c083cb81a85acac3b03241548b39862d61df40
Linh <phamlinh@grinnell.edu>
2017-01-12 22:15:23 -0600

    Update

--- HelloWorldApp/app/src/main/java/app/linh/com/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/app/linh/com/helloworldapp/MainActivity.java
@@ -3,7 +3,11 @@ package app.linh.com.helloworldapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.hello_text_view);
-
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_LONG).show();
+                String message = "HELLO WORLD!!!";
+                textView.setText(message);
+            }
+        });
     }
 
-    public void showGreetings(View view) {
-        String message = "HELLO WORLD!!!";
-        textView.setText(message);
-
-    }
 }
